f567e602e9f623dbdebb5a497718658c20eab970
JoaoVictorHengler <joao.hengler@sptech.school>
2022-05-28 02:10:15 -0300

    Arrumando html

--- Site/public/assets/js/funcoes.js
+++ Site/public/assets/js/funcoes.js
@@ -1,37 +1,3 @@
-function showMenu() {
-    document.getElementById('perfil-menu').style.width = '19vw';
-    document.getElementById('search-user').style.width = '100%';
-    document.getElementsByClassName('user-info')[0].classList.add('user-info-show');
-    search.style.left = '6%';
-    home.classList.add('nav-bar-name-opacity');
-    mapa.classList.add('nav-bar-name-opacity');
-    ranking.classList.add('nav-bar-name-opacity');
-    ultimos.classList.add('nav-bar-name-opacity');
-    login.classList.add('nav-bar-name-opacity');
-    cadastro.classList.add('nav-bar-name-opacity');
-    document.getElementById('user').style.display = 'block';
-    setTimeout(() => {
-        document.getElementById('search-user').placeholder = 'Pesquisar...';
-    }, 500)
-}
-
-function hideMenu() {
-    document.getElementById('perfil-menu').style.width = '5vw';
-    document.getElementById('search-user').style.width = '90%';
-    document.getElementsByClassName('user-info')[0].classList.remove('user-info-show');
-    search.style.left = '14%';
-    home.classList.remove('nav-bar-name-opacity');
-    mapa.classList.remove('nav-bar-name-opacity');
-    ranking.classList.remove('nav-bar-name-opacity');
-    ultimos.classList.remove('nav-bar-name-opacity');
-    login.classList.remove('nav-bar-name-opacity');
-    cadastro.classList.remove('nav-bar-name-opacity');
-    document.getElementById('user').style.display = 'none';
-    setTimeout(() => {
-        document.getElementById('search-user').placeholder = '...';
-    }, 500)
-}
-
 function showPassword() {
     let cadeadoElement = document.getElementById('cadeado');
     let senhaElement = document.getElementById('senha');
