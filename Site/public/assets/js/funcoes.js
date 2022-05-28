17793dc66b7dddd8cbba6eb6e7d59f4a0d29b8ff
JoaoVictorHengler <joao.hengler@sptech.school>
2022-05-27 21:45:05 -0300

    Mudando o estilo das páginas html

--- Site/public/assets/js/funcoes.js
+++ Site/public/assets/js/funcoes.js
@@ -2,7 +2,7 @@ function showMenu() {
     document.getElementById('perfil-menu').style.width = '19vw';
     document.getElementById('search-user').style.width = '100%';
     document.getElementsByClassName('user-info')[0].classList.add('user-info-show');
-    search.style.left = '40px';
+    search.style.left = '6%';
     home.classList.add('nav-bar-name-opacity');
     mapa.classList.add('nav-bar-name-opacity');
     ranking.classList.add('nav-bar-name-opacity');
@@ -19,7 +19,7 @@ function hideMenu() {
     document.getElementById('perfil-menu').style.width = '5vw';
     document.getElementById('search-user').style.width = '90%';
     document.getElementsByClassName('user-info')[0].classList.remove('user-info-show');
-    search.style.left = '15px';
+    search.style.left = '14%';
     home.classList.remove('nav-bar-name-opacity');
     mapa.classList.remove('nav-bar-name-opacity');
     ranking.classList.remove('nav-bar-name-opacity');
@@ -66,12 +66,32 @@ function showRewritePassword() {
 
 function goToLoginOrSingUp(type) {
     if (type == 'login') {
-        window.location.href= "./login.html"
+        window.location.href = "./login.html"
     } else {
-        window.location.href= "./cadastro.html"
+        window.location.href = "./cadastro.html"
     }
 }
 
 function startBackgroundVideo() {
     document.getElementsByClassName('background-gif')[0].play;
+}
+
+function setPlayerCountry(elementFlag, showName, countryName = 'Brasil') {
+    for (let i = 0; i < elementFlag.length; i++) {
+        let countryFlagUnicode = 'U+1F1E7 U+1F1F7'.split(' ');
+
+        countryFlagUnicode.forEach((unicode) => {
+            let convert = twemoji.convert.fromCodePoint(unicode.substring(2));
+            elementFlag[i].textContent += convert;
+        })
+
+        twemoji.parse(document.body);
+        if (showName) {
+            let spanElement = document.createElement('span');
+            spanElement.innerHTML = countryName;
+            spanElement.classList.add('contry-name');
+            elementFlag[i].appendChild(spanElement);
+        }
+    }
+
 }
