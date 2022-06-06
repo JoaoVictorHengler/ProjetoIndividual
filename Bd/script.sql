cb82848ae10a44ca68ce57215e749027778e5cd7
JoaoVictorHengler <joao.hengler@sptech.school>
2022-06-05 23:57:42 -0300

    Atualizando o banco de dados

--- Bd/script.sql
+++ Bd/script.sql
@@ -72,48 +72,13 @@ select * from score;
 
 alter table jogador rename column OculosDeVrUsado to vrUtilizado;
 
-select idJogador, nome, pais,
-    ROW_NUMBER() OVER (order by	sum(pontosDePerformace) desc) as 'rank',
-    (select round(avg(round(pontuacao / maxPontuacao * 100, 2)),2 ) as 'precisao' from Dificuldade 
-    join score on idDificuldade = fkDificuldade and score.fkMapa = dificuldade.fkMapa
-    and Jogador.idJogador = fkJogador group by fkJogador) as 'precisaoMedia',
-    (select count(*) from Dificuldade 
-    join score on idDificuldade = fkDificuldade and score.fkMapa = dificuldade.fkMapa
-    and Jogador.idJogador = fkJogador group by fkJogador) as 'mapasJogados',
-    (select sum(pontosDePerformace) from Dificuldade 
-    join score on idDificuldade = fkDificuldade and score.fkMapa = dificuldade.fkMapa
-    and Jogador.idJogador = fkJogador group by fkJogador) as 'ppTotal',
-    (select count(*) from Dificuldade 
-    join score on idDificuldade = fkDificuldade and score.fkMapa = dificuldade.fkMapa
-    and Jogador.idJogador = fkJogador and round((pontuacao / maxPontuacao) * 100, 2) >= 90 group by fkJogador) as 'SS',
-    (select count(*) from Dificuldade 
-    join score on idDificuldade = fkDificuldade and score.fkMapa = dificuldade.fkMapa
-    and Jogador.idJogador = fkJogador and round((pontuacao / maxPontuacao) * 100, 2) >= 80 and round((pontuacao / maxPontuacao) * 100, 2) < 90 group by fkJogador) as 'S',
-    (select count(*) from Dificuldade 
-    join score on idDificuldade = fkDificuldade and score.fkMapa = dificuldade.fkMapa
-    and Jogador.idJogador = fkJogador and round((pontuacao / maxPontuacao) * 100, 2) >= 65 and round((pontuacao / maxPontuacao) * 100, 2) < 80 group by fkJogador) as 'A'
-    from Jogador join score on idJogador = fkJogador and pais = 'BR'group by idJogador order by ppTotal desc;
-
-select idJogador, count(*) from Score where Jogador.idJogador = fkJogador and precisao >= 90 group by fkJogador;
-select * from jogador;
-
 use gurasaber;
 
-select * from jogador;
-select nome, pais, idScoresaber, vrUtilizado,
-(select round(avg(round(pontuacao / maxPontuacao * 100, 2)),2 ) as 'precisao' from Dificuldade 
-    join score on idDificuldade = fkDificuldade and score.fkMapa = dificuldade.fkMapa
-    and Jogador.idJogador = fkJogador group by fkJogador) as 'precisaoMedia',
-    (select sum(pontosDePerformace) from Dificuldade 
-    join score on idDificuldade = fkDificuldade and score.fkMapa = dificuldade.fkMapa
-    and Jogador.idJogador = fkJogador group by fkJogador) as 'ppTotal',
-    (select ROW_NUMBER() OVER (order by	sum(pontosDePerformace) desc)
-from Jogador j1 join score on j1.idJogador = fkJogador and idJogador = Jogador.idJogador group by idJogador order by rankGlobal) as 'rankGlobal'
-from Jogador where idJogador = 2 group by idJogador;
-
-select ROW_NUMBER() OVER (order by	sum(pontosDePerformace) desc) as 'rankGlobal'
-from Jogador j1 join score on j1.idJogador = fkJogador and idJogador = Jogador.idJogador group by idJogador order by rankGlobal;
-
-select * from Historico;
-  
-
+select * from Jogador;
+select * from mapa;
+select pontuacao, round((pontuacao / maxPontuacao) * 100, 2) as precisao, comboMaximo, corteRuim, notasErradas, dataScore, maxPontuacao, notas, nomeDificuldade,
+nomeMusica, subNomeMusica, criadorMapa, artistaMusica
+from Score 
+                      join Jogador on fkJogador = idJogador and idJogador = 2
+                      join Dificuldade on fkDificuldade = idDificuldade
+                      join Mapa on idMapa = Dificuldade.fkMapa and idMapa = Score.fkMapa order by dataScore desc;
